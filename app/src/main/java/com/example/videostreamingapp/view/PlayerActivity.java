bddf8d8af2dd7134c115e5cded9c64d22ce25a10
xexa <arberi.fazlija@gmail.com>
2020-06-11 19:43:21 +0200

    Added VideoPlayer

--- app/src/main/java/com/example/videostreamingapp/view/PlayerActivity.java
+++ app/src/main/java/com/example/videostreamingapp/view/PlayerActivity.java
@@ -1,7 +1,12 @@
 package com.example.videostreamingapp.view;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -10,11 +15,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.videostreamingapp.R;
 import com.example.videostreamingapp.model.Video;
 
+import retrofit2.http.Url;
+
 public class PlayerActivity extends AppCompatActivity {
 
     private TextView title;
     private TextView description;
     private VideoView videoPlayer;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class PlayerActivity extends AppCompatActivity {
         title = findViewById(R.id.video_title_player);
         description = findViewById(R.id.videoDesc_player_activity);
         videoPlayer = findViewById(R.id.videoView);
+        progressBar = findViewById(R.id.progressBar);
 
         Intent intent = getIntent();
 
@@ -35,6 +44,24 @@ public class PlayerActivity extends AppCompatActivity {
         assert video != null;
         title.setText(video.getTitle());
         description.setText(video.getDescription());
+
+        getSupportActionBar().setTitle(video.getTitle());
+
+        Uri videoUri = Uri.parse(video.getSources().get(0));
+
+        videoPlayer.setVideoURI(videoUri);
+
+        MediaController mediaController = new MediaController(this);
+
+        videoPlayer.setMediaController(mediaController);
+
+        videoPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoPlayer.start();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
 
 
     }
